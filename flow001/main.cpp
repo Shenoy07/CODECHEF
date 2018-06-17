64bb8d373b2070c004c94ddf67c67b1368f9274d
Nikhil Shenoy <nns1998@gmail.com>
2018-06-17 21:18:14 +0530

    added addtion program  /n makes a huge difference as well as the input

---
+++ flow001/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int a,b;
+
+   scanf("%d",&T);
+
+   while(T--)
+       {scanf("%d %d",&a,&b);
+       int c=a+b;
+        printf("%d\n",c);
+   }
+return 0;
+}
